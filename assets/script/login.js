6e69bba094b8c7b47374779a0eb8d8407c2ce659
farrahfahira <farrahfahira2202@gmail.com>
2022-10-21 20:52:03 +0700

    menambahkan file js utk login

---
+++ assets/script/login.js
@@ -0,0 +1,17 @@
+const modal = document.querySelector(".popup");
+const trigger = document.querySelector(".login-btn");
+const closeButton = document.querySelector(".close-btn");
+
+function toggleModal() {
+  modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+  if (event.target === modal) {
+    toggleModal();
+  }
+}
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
